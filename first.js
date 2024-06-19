b7fcc075a58c378fd66925b56a8e347a4884f240
Manav1234-web <manav.ackplus@gmail.com>
2024-06-19 18:34:06 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,90 @@
+// Artimetic Operators
+// let a = 5;
+// let b = 2;
+// console.log(" a = " , a ," b = " , b);
+// console.log(" a + b = ",a + b );
+// console.log(" a - b = ",a - b );
+// console.log(" a * b = ",a * b );
+// console.log(" a / b = ",a / b );
+// console.log(" a % b = ",a % b );
+// console.log(" a ** b = ",a ** b );
+
+// Unary Operators
+// let a = 5;
+// let b = 2;
+// console.log(" a = " , a ," b = " , b);
+// a = a + 1;
+// console.log("a = ", a)
+// a = a - 1;
+// console.log("a = ", a)
+
+// Assignment Operators
+// let a = 5;
+// let b = 2;
+// a += 4;
+// console.log("a = ,", a )
+
+// Comparison Operators
+// let a = 5;
+// let b = 2;
+
+// console.log(" 5 == 2 ",  a == b);
+
+// Logical Operators
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;
+// let cond2 = a === 6;
+// console.log(" cond1 && cond2 = " , cond1 && cond2) ;
+
+// Ternary Operators
+//  let age = 16 ;
+
+// let result = age >=18 ? "adult" : "not adult" ;
+// console.log(result)
+
+// let name = prompt("hello..!");
+// console.log(name);
+
+// let num = prompt("Enter a number");
+
+// if( num % 5 === 0) {
+//     console.log(num , "is a multiple of 5 ");
+// }  else{
+//     console.log(num , "is not a multiple of 5 ");
+// }
+
+// practice Qu2 J-S program
+
+// let score = prompt("enter your score ( 0-100):");
+// let grade;
+
+// if( score >= 90 && score <=100) {
+//     grade = "A" ;
+// }  else if ( score >= 70 && score <=89){
+//     grade = "B" ;
+// }  else if ( score >=60 && score <=69){
+//     grade = "C"
+// }  else if ( score >=50 && score <=59){
+//     grade = "E"
+// }  else if ( score >=0 && score <=49){
+//     grade = "F"
+// }
+
+// console.log("according to your scores , your grade was :" , grade )
+
+// Loops & strings
+
+// for (let count = 1 ; count <= 5 ; count++) {
+//     console.log("Apna College") ;
+// }
+// console.log("Loops has ended");
+
+// let sum = 0;
+// for (let i=1; i<=5; i++) {
+//   sum = sum + i;
+// }
+
+// console.log("sum = ", sum );
+// console.log("Loops has ended");
